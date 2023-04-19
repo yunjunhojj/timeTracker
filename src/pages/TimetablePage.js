28406126957db3fb0ff87cb0fd39e0d33597a300
junho <yunjunhojj@gmail.com>
2023-04-20 02:10:22 +0900

    style: timetable basic style

--- src/pages/TimetablePage.js
+++ src/pages/TimetablePage.js
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import { Container, Grid, TextField, Typography } from "@mui/material";
+import styled from "styled-components";
+
+const TimetablePageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  padding-bottom: 40px;
+`;
+
 const TimetablePage = () => {
-  return <>TimetablePage</>;
+  const [timetable, setTimetable] = useState(Array(24).fill(""));
+
+  const handleChange = (event, index) => {
+    const newTimetable = [...timetable];
+    newTimetable[index] = event.target.value;
+    setTimetable(newTimetable);
+  };
+
+  return (
+    <TimetablePageWrapper>
+      <Container maxWidth="sm" sx={{ marginTop: "2rem" }}>
+        <Typography variant="h4" align="center" sx={{ marginBottom: "2rem" }}>
+          Today's Time Table
+        </Typography>
+        <Grid container spacing={2}>
+          {[...Array(24)].map((_, index) => (
+            <Grid item xs={4} key={index}>
+              <TextField
+                fullWidth
+                label={`${index}:00 - ${index + 1}:00`}
+                variant="outlined"
+                value={timetable[index]}
+                onChange={(event) => handleChange(event, index)}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </TimetablePageWrapper>
+  );
 };
 
 export default TimetablePage;
