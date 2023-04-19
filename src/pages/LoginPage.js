11807945a4a043fe211d6812f839c460e97fb109
junho <yunjunhojj@gmail.com>
2023-04-20 00:05:40 +0900

    style:  Auth page styled component

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./LoginPage.css"; // import any necessary styles
+import styled from "styled-components";
 
 function LoginPage() {
   const [email, setEmail] = useState("");
@@ -21,29 +20,82 @@ function LoginPage() {
   };
 
   return (
-    <div className="login-page">
-      <h1>Time-Tracker</h1>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </label>
-        <label>
-          Password:
-          <input
+    <LoginPageContainer>
+      <Title>Time-Tracker</Title>
+      <Subtitle>Login</Subtitle>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label>Email:</Label>
+          <Input type="email" value={email} onChange={handleEmailChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label>Password:</Label>
+          <Input
             type="password"
             value={password}
             onChange={handlePasswordChange}
           />
-        </label>
-        <button type="submit">Login</button>
-      </form>
+        </FormGroup>
+        <Button type="submit">Login</Button>
+      </Form>
       <p>
-        Don't have an account? <Link to="/signup">Sign up</Link>
+        아이디가 없으신가요? <a href="/signup">Sign up</a>
       </p>
-    </div>
+    </LoginPageContainer>
   );
 }
+
+const LoginPageContainer = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 40px;
+  background-color: #f8f8f8;
+`;
+
+const Title = styled.h1`
+  font-size: 36px;
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #fff;
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 export default LoginPage;
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,98 @@
-const SignUpPage = () => {
-  return <>SignUpPage</>;
-};
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const SignUpPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+`;
+
+const SignUpForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const SignUpInput = styled.input`
+  padding: 10px;
+  margin-bottom: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  font-size: 16px;
+  font-weight: bold;
+  width: 100%;
+`;
+
+const SignUpButton = styled.button`
+  background-color: #0077c0;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // implement sign up functionality here
+  };
+
+  return (
+    <SignUpPageWrapper>
+      <SignUpForm onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <SignUpInput
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+        <SignUpInput
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+        <SignUpInput
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          required
+        />
+        <SignUpButton type="submit">Sign Up</SignUpButton>
+      </SignUpForm>
+    </SignUpPageWrapper>
+  );
+}
 
 export default SignUpPage;
