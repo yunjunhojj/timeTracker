3b16b85233298f80f98cc0b9849b8810e76c9113
junho <yunjunhojj@gmail.com>
2023-04-20 03:16:30 +0900

    fix : styled component outter position

--- src/components/layouts/TopNavigation.js
+++ src/components/layouts/TopNavigation.js
@@ -2,59 +2,59 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #0077cc;
+  color: #fff;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    height: auto;
+  }
+`;
+
+const NavTitle = styled.h1`
+  margin: 0;
+`;
+
+const NavLinks = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 1rem;
+  }
+`;
+
+const NavLink = styled.li`
+  margin-left: 1rem;
+
+  & > a {
+    color: #fff;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  @media (max-width: 768px) {
+    margin: 0.5rem 0;
+  }
+`;
+
 const TopNavigation = () => {
   const [isLogged, setIsLogged] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(true);
   const [isMobile, setIsMobile] = useState(false);
-
-  const NavBar = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    background-color: #0077cc;
-    color: #fff;
-
-    @media (max-width: 768px) {
-      flex-direction: column;
-      justify-content: center;
-      height: auto;
-    }
-  `;
-
-  const NavTitle = styled.h1`
-    margin: 0;
-  `;
-
-  const NavLinks = styled.ul`
-    display: flex;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-
-    @media (max-width: 768px) {
-      flex-direction: column;
-      align-items: center;
-      margin-top: 1rem;
-    }
-  `;
-
-  const NavLink = styled.li`
-    margin-left: 1rem;
-
-    & > a {
-      color: #fff;
-      text-decoration: none;
-
-      &:hover {
-        text-decoration: underline;
-      }
-    }
-
-    @media (max-width: 768px) {
-      margin: 0.5rem 0;
-    }
-  `;
 
   useEffect(() => {
     const viewPortWidth = window.innerWidth;
